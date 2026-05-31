0b78b918ac2294602036dadedcb740f563c16ff3
denjisensei4-boop <denjisensei4@gmail.com>
2026-05-31 19:32:34 +0530

    fix: downgrade to stable vite 5 and standard react plugin

--- resturent-refurds/vite.config.ts
+++ resturent-refurds/vite.config.ts
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", { target: "18" }],
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
+  server: {
+    port: 5173
+  }
 })
